c70d6e01c86bc2e0a3ab67e95e57113b31c79c7e
Tom Ruff <tomruffcodes@gmail.com>
2023-12-14 21:01:48 -0700

    started adding all the inputs and buttons in the JS

--- js/actions.js
+++ js/actions.js
@@ -1,3 +1,14 @@
+// containers
+const welcome_section_elm = $("#welcome_section")
+const signup_section_elm = $("#signup_section")
+const baby_settings_modal_elm = $("#baby_settings_modal")
+const baby_add_data_modal_elm = $("#baby_add_data_modal")
+// inputs
+const full_name_input = $("#full_name_input")
+const birthday_input = $("#birthday_input")
+// buttons
+const get_started_btn = $("#get_started_btn")
+
 $(()=>{
     
 })
--- js/app.js
+++ js/app.js
@@ -1,7 +1,45 @@
+// example data
+
+var babies = [
+
+    {
+        _id:"",
+        name: "",
+        birthday: "",
+        gender: "",
+        bm_data: [],
+        weight_data: [],
+        feed_data: [],
+    }
+]
+
+var bm_data = {
+    _id:"",
+    type:"",
+    note:"",
+    captured:"",
+}
+
+var weight_data = {
+    _id:"",
+    weight:"",
+    captured:"",
+}
+
+var feed_data = {
+    _id:"",
+    amount:"",
+    duration_hr:"",
+    duration_min:"",
+    captured:"",
+}
+
+
+
 // on load
 // Need to create baby user
-const createBabyUser = ()=>{
-    
+const createBabyUser = () => {
+
 }
 
 
@@ -13,6 +51,6 @@ const createBabyUser = ()=>{
 
 
 
-$(()=>{
-    
+$(() => {
+
 })
