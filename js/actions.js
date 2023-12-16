4caac8a6fad0b21f0e28feb895f5d58b53c07480
Tom Ruff <tomruffcodes@gmail.com>
2023-12-16 12:08:12 -0700

    adding a new baby is finished and looking good

--- js/actions.js
+++ js/actions.js
@@ -5,6 +5,9 @@ const baby_add_data_modal_elm = $("#baby_add_data_modal")
 const full_name_input = $("#full_name_input")
 const birthday_input = $("#birthday_input")
 const gender_select = $("#gender_select")
+const add_new_baby_input_name = $("#add_new_baby_input_name")
+const add_new_baby_input_birthday = $("#add_new_baby_input_birthday")
+const add_new_baby_input_gender = $("#add_new_baby_input_gender")
 // buttons
 const get_started_btn = $("#get_started_btn")
 const sign_first_baby_up_btn = $("#sign_first_baby_up_btn")
@@ -14,7 +17,8 @@ const add_new_baby_btn = $("#add_new_baby_btn")
 
 
 // create functions
-const createBabyUser = (values) => {
+const createBabyUser = (newBaby, values) => {
+
     let baby = {
         _id: createId(),
         name: values.name,
@@ -26,7 +30,17 @@ const createBabyUser = (values) => {
         feed_data: [],
     }
     // save global baby data
-    globalBabyData = []
+    if (newBaby) {
+        //if this is the start of adding a baby, initialize the global data
+        globalBabyData = []
+    }
+    if (globalBabyData.length >= 1) {
+        // set other active babies to false
+        globalBabyData.forEach(b => {
+            b.active = false
+        });
+    }
+    // after setting other babies to not active now push new baby
     globalBabyData.push(baby)
 
     //save to local storage
@@ -36,7 +50,9 @@ const createBabyUser = (values) => {
     // load html
     loadHtml()
     console.log(globalBabyData)
-
+    createNotification(`${baby.name} Has been added!`)
+    // set state to true
+    babyHasBeenAdded = true
 }
 
 
@@ -45,7 +61,7 @@ const createBabyUser = (values) => {
 let babyName = ""
 let babyBirthday = ""
 let babyGender = ""
-
+let babyHasBeenAdded = false;
 
 
 $(() => {
@@ -69,9 +85,21 @@ $(() => {
         babyGender = e.target.value
 
     })
-    // first sign up for baby
-    $(sign_first_baby_up_btn).on("click", (e) => {
-        e.preventDefault()
+    // reset values
+    const clearNewBabyValues = () => {
+        babyName = ""
+        babyBirthday = ""
+        babyGender = ""
+        // clear inputs
+        $(full_name_input).val('')
+        $(birthday_input).val('')
+        $(gender_select).val('')
+        $(add_new_baby_input_name).val('')
+        $(add_new_baby_input_birthday).val('')
+        $(add_new_baby_input_gender).val('')
+    }
+
+    const gatherNewBabyDataAndCreate = (firstBaby) => {
         let babyValues = {}
         babyValues.name = babyName
         babyValues.birthday = babyBirthday
@@ -88,9 +116,15 @@ $(() => {
             createNotification("Please enter the Baby's gender.")
             return
         }
-
-        console.log(babyValues)
-        createBabyUser(babyValues)
+        // create new baby
+        createBabyUser(firstBaby, babyValues)
+        //clear values
+        clearNewBabyValues()
+    }
+    // first sign up for baby
+    $(sign_first_baby_up_btn).on("click", (e) => {
+        e.preventDefault()
+        gatherNewBabyDataAndCreate(true)
     })
 
     // open settings modal
@@ -106,14 +140,55 @@ $(() => {
         // open up settings
         $(baby_settings_modal_elm).fadeOut()
         $(baby_settings_modal_elm).removeClass('active')
+        // close form if open
+        $("#add_baby_form").hide()
     })
     // theme toggle
     $("#theme_toggle_btn").on('click', () => {
         toggleTheme()
     })
 
-    $(add_new_baby_btn).on(click,(e)=>{
+    $(add_new_baby_btn).on('click', (e) => {
         e.preventDefault()
-        
+        $("#add_baby_form").fadeIn()
+        $(add_new_baby_btn).hide()
     })
+    $("#add_new_baby_cancel_btn").on('click', (e) => {
+        e.preventDefault()
+        // hide form
+        $("#add_baby_form").hide()
+        //show add baby button
+        // clear contents in form
+        $(add_new_baby_btn).fadeIn()
+        clearNewBabyValues()
+    })
+
+    add_new_baby_input_name.on('keyup change', (e) => {
+        babyName = e.target.value
+    })
+    add_new_baby_input_birthday.on('keyup change', (e) => {
+        babyBirthday = e.target.value
+
+    })
+    add_new_baby_input_gender.on('keyup change', (e) => {
+        babyGender = e.target.value
+
+    })
+
+    $("#add_new_baby_add_btn").on("click", (e) => {
+        e.preventDefault()
+        gatherNewBabyDataAndCreate(false)
+        if (babyHasBeenAdded === true) {
+            // close form
+            // hide form
+            $("#add_baby_form").hide()
+            //show add baby button
+            // clear contents in form
+            $(add_new_baby_btn).fadeIn()
+            // reset state
+            babyHasBeenAdded = false
+        }
+
+    })
+
 })
