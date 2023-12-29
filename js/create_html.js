f6e3e9fe37e8d73f79d8a3e93c7271b1afc32193
Tom Ruff <ruffmantom@gmail.com>
2023-12-29 15:24:01 -0700

    created the chart as html instead of vector and works like a charm. need to adjust the time Y coordinates though

--- js/create_html.js
+++ js/create_html.js
@@ -1,17 +1,17 @@
 const dayCoordinates = {
-    sunday: -1605,
-    monday: -1554,
-    tuesday: -1501,
-    wednesday: -1448,
-    thursday: -1394,
-    friday: -1346,
-    saturday: -1303,
+    sunday: 3,
+    monday: 52,
+    tuesday: 103,
+    wednesday: 153,
+    thursday: 204,
+    friday: 249,
+    saturday: 294,
 }
 
 // min and max coordinates
 const minAndMaxCoordinates = {
-    yMinHeight: 1372,
-    yMaxHeight: 1093,
+    yMinHeight: 263,
+    yMaxHeight: 0,
 }
 
 const returnDayOfWeekCoordinate = (dateAndTime) => {
@@ -70,18 +70,9 @@ const returnCircleCoordinates = (data) => {
     // coordinates object
     let coordinatesObj = {
         innerX: returnDayOfWeekCoordinate(data),
-        innerY: returnTimeOfDayCoordinate(time),
+        innerY:Math.round( returnTimeOfDayCoordinate(time)),
     };
 
-    // Calculate inner coordinates based on outer coordinates
-    coordinatesObj.outerX = coordinatesObj.innerX - 2;
-    coordinatesObj.outerY = coordinatesObj.innerY - 2;
-
-    // Add coordinates strings to the object
-    coordinatesObj.outerCoor = `${coordinatesObj.outerX} ${Math.round(coordinatesObj.outerY)}`;
-    coordinatesObj.innerCoor = `${coordinatesObj.innerX} ${Math.round(coordinatesObj.innerY)}`;
-
-    console.log("returnCircleCoordinates RETURN coordinatesObj: ", coordinatesObj);
     return coordinatesObj;
 };
 
@@ -90,98 +81,35 @@ const returnPottyDotColor = (type) => {
     let color;
     switch (type) {
         case "potty":
-            color = "#fd80ab"
+            color = "potty_dot"
             break;
         case "poopy":
-            color = "#64b5f6"
+            color = "poopy_dot"
             break;
 
         default:
-            color = "#b364f6"
+            color = "two_for_one_dot"
     }
     return color;
 }
 
-// const createSVG(tag) {
-//     return document.createElementNS('http://www.w3.org/2000/svg', tag);
-// }
 
+const createBmDot = (bm, index) => {
+    // these x coordinates are where the outer circle lands in the middle of the text.
+    let coordinates = returnCircleCoordinates(bm.created_at)
+    let typeClassName = returnPottyDotColor(bm.type)
+    console.log(coordinates.innerX)
+    console.log(coordinates.innerY)
+    return `
+        <div id="data_dot_${index}" class="bm_dot_outer ${typeClassName} chart_dot" style="top:${coordinates.innerY}px; left:${coordinates.innerX}px;">
+            <div class="bm_dot_inner ${typeClassName}"></div>
+        </div>
+    `
+}
 
-// const createBmDot = (bm, index) => {
-//     // these x coordinates are where the outer circle lands in the middle of the text.
-//     let coordinates = returnCircleCoordinates(bm.created_at)
-//     let typeColor = returnPottyDotColor(bm.type)
-//     // return `
-//     //     <g id="bm_dot_${index + 1}">
-//     //         <circle id="Ellipse_9-8" data-name="Ellipse 9" cx="4" cy="4" r="4" transform="translate(${coordinates.innerCoor})" fill="${typeColor}"></circle>
-//     //         <circle id="Ellipse_10-8" data-name="Ellipse 10" cx="6" cy="6" r="6" transform="translate(${coordinates.outerCoor})" fill="none" stroke="${typeColor}" stroke-width="1"></circle>
-//     //     </g>
-//     // `
-
-//     var newSvgContent = `<g id="potty-dot">` +
-//         `<circle id="Ellipse_9" data-name="Ellipse 9" cx="4" cy="4" r="4" transform="translate(${coordinates.innerCoor})" fill="${typeColor}"/>` +
-//         `<circle id="Ellipse_10" data-name="Ellipse 10" cx="6" cy="6" r="6" transform="translate(${coordinates.outerCoor})" fill="none" stroke="${typeColor}" stroke-width="1"/>` +
-//         '</g>';
-
-//     // Create a temporary div element
-//     var tempDiv = document.createElement('div');
-//     tempDiv.innerHTML = newSvgContent;
-
-//     // Get the newly created SVG element from the temporary div
-//     var newSvgElement = tempDiv.firstElementChild;
-
-//     // Get the existing SVG container
-
-// }
-
-
-// const create_BM_chart_HTML = (baby) => {
-//     baby.bm_data.forEach((bm, index) => {
-//         let coordinates = returnCircleCoordinates(bm.created_at)
-//         let typeColor = returnPottyDotColor(bm.type)
-//         var newSvgContent = `<g id="potty_dot_${index}">` +
-//             `<circle id="Ellipse_9" data-name="Ellipse 9" cx="4" cy="4" r="4" transform="translate(${coordinates.innerCoor})" fill="${typeColor}"/>` +
-//             `<circle id="Ellipse_10" data-name="Ellipse 10" cx="6" cy="6" r="6" transform="translate(${coordinates.outerCoor})" fill="none" stroke="${typeColor}" stroke-width="1"/>` +
-//             '</g>';
-
-//         // Create a temporary div element
-//         var tempDiv = document.createElement('div');
-//         tempDiv.innerHTML = newSvgContent;
-//         // Get the newly created SVG element from the temporary div
-//         var newSvgElement = tempDiv.firstElementChild;
-//         // $("#bm_dot_data").append(createBmDot(bm, index))
-//         let mainSvg = document.getElementById('bm_dot_data');
-//         // Append the new SVG element to the existing SVG container
-//         mainSvg.appendChild(newSvgElement);
-//     });
-// }
 
 const create_BM_chart_HTML = (baby) => {
     baby.bm_data.forEach((bm, index) => {
-        let coordinates = returnCircleCoordinates(bm.created_at);
-        let typeColor = returnPottyDotColor(bm.type);
-        console.log('Coordinates:', coordinates);
-        console.log('Type Color:', typeColor);
-
-        var newSvgContent = `<g class="potty_dot_${index}">` +
-            `<circle id="Ellipse_9" data-name="Ellipse 9" cx="4" cy="4" r="4" transform="translate(${coordinates.innerCoor})" fill="${typeColor}"/></circle>` +
-            `<circle id="Ellipse_10" data-name="Ellipse 10" cx="6" cy="6" r="6" transform="translate(${coordinates.outerCoor})" fill="none" stroke="${typeColor}" stroke-width="1"/></circle>` +
-            '</g>';
-
-        console.log('New SVG Content:', newSvgContent);
-        
-        // Create a temporary div element
-        var tempDiv = document.createElement('div');
-        tempDiv.innerHTML = newSvgContent;
-        
-        // Get the newly created SVG element from the temporary div
-        var newSvgElement = tempDiv.firstElementChild;
-        
-        // Get the existing SVG group with the class 'bm_dot_data'
-        let bmDotDataGroup = document.getElementById('bm_dot_data');
-        console.log('bmDotDataGroup:', bmDotDataGroup);
-
-        // Append the new SVG element to the existing SVG group with the class 'bm_dot_data'
-        bmDotDataGroup.appendChild(newSvgElement);
+        $("#dot_cont").append(createBmDot(bm,index))
     });
 }
